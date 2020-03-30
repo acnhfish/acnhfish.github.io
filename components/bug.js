4ff87829792bf50667b61511f1c2b49b58c34c9f
acnhfish <nicholas.josue@icloud.com>
2020-03-29 21:56:02 -0500

    More component stuff
    Fleshed out data representation in components

--- components/bug.js
+++ components/bug.js
@@ -8,7 +8,7 @@ Vue.component("bug", {
         <span class="location">{{ bug.Location }}</span>
         <span class="availability-time" v-if="bug.StartTime != 0">{{ bug.StartTime }}&#58;00  -{{ bug.EndTime }}&#58;00</span>
         <span class="availability-time" v-else>Any time</span>
-        <span class="availability-date">date</span>
+        <span class="availability-date" v-for="month in bug.Months" v-bind:class="{ available: month }">{{ month }}</span>
         <span class="value">Bells: {{ bug.Value }}</span>
     </div>`
 });
--- components/fish.js
+++ components/fish.js
@@ -9,7 +9,7 @@ Vue.component("fish", {
         <span class="fish-shadow">{{ fish.ShadowType }}</span>
         <span class="availability-time" v-if="fish.StartTime != 0">{{ fish.StartTime }}&#58;00-{{ fish.EndTime }}&#58;00</span>
         <span class="availability-time" v-else>Any time</span>
-        <span class="availability-date">date</span>
-        <span class="value">{{ fish.Value }}</span>
+        <span class="availability-date" v-for="month in fish.Months" v-bind:class="{ available: month }">{{ month }}</span>
+        <span class="value">Bells: {{ fish.Value }}</span>
     </div>`
 });
