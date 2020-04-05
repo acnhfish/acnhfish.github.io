e15777d19e630af13dcaa4cc2a596f56ca865876
acnhfish <nicholas.josue@icloud.com>
2020-04-05 16:32:18 -0500

    More styling stuff

--- components/bug.js
+++ components/bug.js
@@ -93,7 +93,7 @@ Vue.component("bug", {
   template: `<div class="critter" v-if="showIfCurrentMonth && showIfCurrentTime && bug.Bug.toLowerCase().includes(this.search)">
         <h4>{{ bug.Bug }}</h4>
         <span class="location">Location: {{ location }}</span>
-        <span class="availability-time" v-if="bug.StartTime != 0">Availability: {{ bug.StartTime }}&#58;00  -{{ bug.EndTime }}&#58;00</span>
+        <span class="availability-time" v-if="bug.StartTime != 0">Availability: {{ bug.StartTime }}&#58;00-{{ bug.EndTime }}&#58;00</span>
         <span class="availability-time" v-else>Availability: Any time</span>
         <div class="datebox" v-if="hemisphere">
         <span class="availability-date" v-for="(month, index) in bug.NorthMonths" v-bind:class="{ available: month }">{{ formatMonth(index) }}</span>
